2c439ff322fabb3b3a79d5a50a607ac8bb3a05ec
SechelTov <83236030+SechelTov@users.noreply.github.com>
2021-09-14 14:46:56 +0300

    Update refet.py

--- refet.py
+++ refet.py
@@ -11,26 +11,25 @@ while True:
         break
 
 nihush = '0000'
-nihush = st.text_input('ניחוש שלך 4 ספרות  ')
-# להוסיף בדיקה שקיבלנו בדיוק מספר של 4 ספרות
-if not nihush.isnumeric() or len(nihush) != 4:
-    st.write('!!!ביקשתי 4 ספרות')
-    continue                                  # תמשיך
+nihush = st.text_input('0000', 'ניחוש שלך 4 ספרות  ')
 
 if nihush == sod:
     st.write('!כל הכבוד')
     break
 
-parot = 0
-shvarim = 0
+if not nihush.isnumeric() or len(nihush) != 4:
+    st.write('!!!ביקשתי 4 ספרות')
+else:
+    parot = 0
+    shvarim = 0
 
-for i, n in enumerate(nihush):    # תמספר
-    for j, s in enumerate(sod):
-        if n == s:
-            if i == j:
-                shvarim = shvarim +1
-            else:                       # אחרת
-                parot = parot+1
+    for i, n in enumerate(nihush):    # תמספר
+        for j, s in enumerate(sod):
+            if n == s:
+                if i == j:
+                    shvarim = shvarim +1
+                else:                       # אחרת
+                    parot = parot+1
 
-st.write('פרות ', parot)
-st.write('שוורים ', shvarim)
+    st.write('פרות ', parot)
+    st.write('שוורים ', shvarim)
