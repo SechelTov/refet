9a5a38ecdeda929d7d4f55dafa6b75c8f78280a9
SechelTov <83236030+SechelTov@users.noreply.github.com>
2021-09-14 14:50:07 +0300

    Update refet.py

--- refet.py
+++ refet.py
@@ -10,7 +10,7 @@ while True:
     if shonot(sod):
         break
 
-nihush = st.text_input('ניחוש שלך 4 ספרות  ', '0000')
+nihush = st.text_input('ניחוש שלך 4 ספרות  ')
 
 if nihush == sod:
     st.write('!כל הכבוד')
